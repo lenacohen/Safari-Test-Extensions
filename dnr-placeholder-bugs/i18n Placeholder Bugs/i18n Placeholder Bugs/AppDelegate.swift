3c4a355e05911b5aa8f2760e2983e99c862ccfba
lenacohen <lcohen@eff.org>
2025-04-22 19:07:47 -0400

    Add demo extension for i18n placeholder bugs

---
+++ dnr-placeholder-bugs/i18n Placeholder Bugs/i18n Placeholder Bugs/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  i18n Placeholder Bugs
+//
+//  Created by Lena Cohen on 4/22/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
